4e219b72250e5c00735f49290a7fc927023df635
safieawaad <148000535+safieawaad@users.noreply.github.com>
2024-01-29 16:37:11 +0200

    Encapsule buttons declarations inside an object

--- main.js
+++ main.js
@@ -1,42 +1,48 @@
+/* ----- Start: Declarations ----- */
 const screenPara = document.getElementById('screen-para');
 const buttonsContainer = document.getElementById('buttons-container');
-const buttonsArray = Array.from(buttonsContainer.querySelectorAll('button'));
-const btnObj = declareButtons(buttonsArray);
+/* ----- End: Declarations ----- */
 
-/* ----- Declare all buttons inside an object ----- */
-function declareButtons(buttonsArray, obj = {}) {
-    let firstRow = 7; let secondRow = 4; let thirdRow = 1;
-    for (let i = 0; i <= 16; i++) {
-        if (i >= 0 && i <= 2) {
-            obj[`btn${firstRow}`] = buttonsArray[i];
-            firstRow++;
-        } else if (i >= 4 && i <= 6) {
-            obj[`btn${secondRow}`] = buttonsArray[i];
-            secondRow++;
-        } else if (i >= 8 && i <= 10) {
-            obj[`btn${thirdRow}`] = buttonsArray[i];
-            thirdRow++;
-        } else if (i === 13) {
-            obj.btn0 = buttonsArray[i];
-        } else if (i === 3) {
-            obj.btnPlus = buttonsArray[i];
-        } else if (i === 7) {
-            obj.btnMinus = buttonsArray[i];
-        } else if (i === 11) {
-            obj.btnTimes = buttonsArray[i];
-        } else if (i === 12) {
-            obj.btnClear = buttonsArray[i];
-        } else if (i === 14) {
-            obj.btnEqual = buttonsArray[i];
-        } else if (i === 15) {
-            obj.btnDivision = buttonsArray[i];
+/* ----- Start: Declare all buttons inside an object ----- */
+const buttonsObj = {
+    array: Array.from(buttonsContainer.querySelectorAll('button')),
+    declareButtons: function (obj = {}) {
+        let firstRow = 7; let secondRow = 4; let thirdRow = 1;
+        for (let i = 0; i <= 16; i++) {
+            if (i >= 0 && i <= 2) {
+                obj[`btn${firstRow}`] = this.array[i];
+                firstRow++;
+            } else if (i >= 4 && i <= 6) {
+                obj[`btn${secondRow}`] = this.array[i];
+                secondRow++;
+            } else if (i >= 8 && i <= 10) {
+                obj[`btn${thirdRow}`] = this.array[i];
+                thirdRow++;
+            } else if (i === 13) {
+                obj.btn0 = this.array[i];
+            } else if (i === 3) {
+                obj.btnPlus = this.array[i];
+            } else if (i === 7) {
+                obj.btnMinus = this.array[i];
+            } else if (i === 11) {
+                obj.btnTimes = this.array[i];
+            } else if (i === 12) {
+                obj.btnClear = this.array[i];
+            } else if (i === 14) {
+                obj.btnEqual = this.array[i];
+            } else if (i === 15) {
+                obj.btnDivision = this.array[i];
+            }
         }
+        return obj;
     }
-    return obj;
 }
+const btn = buttonsObj.declareButtons();
+/* ----- End: Declare all buttons inside an object ----- */
 
-/* ----- Operations Functions ----- */
+/* ----- Start: Operations Functions ----- */
 function addNumbers(num1, num2) { return num1 + num2 }
 function subtractNumbers(num1, num2) { return num1 - num2 }
 function multiplyNumbers(num1, num2) { return num1 * num2 }
 function divideNumbers(num1, num2) { return num1 / num2 }
+/* ----- End: Operations Functions ----- */
