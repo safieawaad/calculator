447b66bdbc1d48e71d85ad2c5d9d40f76ed0a552
safieawaad <148000535+safieawaad@users.noreply.github.com>
2024-02-03 13:04:10 +0200

    Add basic operations functions

---
+++ main.js
@@ -0,0 +1,5 @@
+/* Operations functions */
+function addNumbers(result, screenNum) { return result + screenNum }
+function subtractNumbers(result, screenNum) { return result - screenNum }
+function multiplyNumbers(result, screenNum) { return result * screenNum }
+function divideNumbers(result, screenNum) { return result / screenNum }
