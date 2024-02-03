47de35e2570ca16e56e58c52f1b85ad828e2ef09
safieawaad <148000535+safieawaad@users.noreply.github.com>
2024-02-03 15:24:06 +0200

    Add operate Function

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ function addNumbers(result, screenNum) { return result + screenNum }
 function subtractNumbers(result, screenNum) { return result - screenNum }
 function multiplyNumbers(result, screenNum) { return result * screenNum }
 function divideNumbers(result, screenNum) { return result / screenNum }
+
+/* This function returns the result */
+function operate(screenNum, result, operator) {
+    if (screenNum && result && operator) {
+        if (operator === '10') return addNumbers(result, screenNum);
+        else if (operator === '11') return subtractNumbers(result, screenNum);
+        else if (operator === '12') return multiplyNumbers(result, screenNum);
+        else if (operator === '13') return divideNumbers(result, screenNum);
+    }
+}
